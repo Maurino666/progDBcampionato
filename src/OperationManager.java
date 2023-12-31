4faaf2ef7d67455b79792a6aa46bfbfab022c7ac
Maurilio LaRocca <maurilio.larocca@gmail.com>
2023-12-31 15:56:16 +0100

    finitura operationManager

--- src/OperationManager.java
+++ src/OperationManager.java
@@ -38,14 +38,14 @@ public class OperationManager extends DBmanager{
     // operazione 1: registrazione di una scuderia
     public int insertScuderia (String nome, String paese){
         lastNumeroGara++;
-        String query = "INSERT INTO scuderia(numeroGara, nome, paese) VALUES " + makeAttributi(Integer.toString(lastNumeroGara), nome, paese);
+        String query = "INSERT INTO scuderia(numeroGara, nome, paese) VALUES " + makeAttributi(Integer.toString(lastNumeroGara), nome, paese) + ";";
         return runUpdate(query);
     }
 
     //operazione 2
     public int insertVettura(String modello, String scuderia){
         
-        String query = "INSERT INTO vettura(modello, scuderia) VALUES" + makeAttributi(modello, scuderia);
+        String query = "INSERT INTO vettura(modello, scuderia) VALUES" + makeAttributi(modello, scuderia) + ";";
         return runUpdate(query); 
 
     }
@@ -65,23 +65,24 @@ public class OperationManager extends DBmanager{
         String costruttore
     ){
         String query = "INSERT INTO costruttore VALUES " + makeAttributi(
-            codice,
-            vettura, 
-            dataInstall, 
-            costo, 
-            tipo, 
-            nCilindri, 
-            tipoMotore, 
-            cilindrata,
-            nMarce,
-            peso,
-            materiale,
-            costruttore);    
+                codice,
+                vettura, 
+                dataInstall, 
+                costo, 
+                tipo, 
+                nCilindri, 
+                tipoMotore, 
+                cilindrata,
+                nMarce,
+                peso,
+                materiale,
+                costruttore
+            ) + ";";    
         return runUpdate(query);
     }
 
     public int incrementCostruttore(String costruttore){
-        String query = "UPDATE costruttore SET nComponenti = nComponenti + 1 WHERE ragioneSociale = " + putApici(costruttore);
+        String query = "UPDATE costruttore SET nComponenti = nComponenti + 1 WHERE ragioneSociale = " + putApici(costruttore) + ";";
         return runUpdate(query);
     }
 
@@ -99,33 +100,33 @@ public class OperationManager extends DBmanager{
     ){
 
         String query = "INSERT INTO pilota(vettura, nome, cognome, dataNascita, nazionalita, tipo, nLicenze, data1Licenza) VALUES "+ makeAttributi(
-            vettura,
-            nome,
-            cognome,
-            dataNascita,
-            nazionalita,
-            tipo,
-            nLicenze,
-            data1Licenza
-        ); 
+                vettura,
+                nome,
+                cognome,
+                dataNascita,
+                nazionalita,
+                tipo,
+                nLicenze,
+                data1Licenza
+            ) + ";"; 
         return runUpdate(query);
     }
 
     //operazione 4: registrazione di un finanziamento per una scuderia
     public int updateFinanziamento(String codicePilota, String quotaFinanziamento){
-        String query = "UPDATE pilota SET quotaFinanziamento = " + quotaFinanziamento + " WHERE codicePilota = " + codicePilota + " AND quotaFinanziamento IS NULL";
+        String query = "UPDATE pilota SET quotaFinanziamento = " + quotaFinanziamento + " WHERE codicePilota = " + codicePilota + " AND quotaFinanziamento IS NULL;";
         return runUpdate(query);
     }
 
     //operazione 5: iscruzione di una vettura ad una gara
     public int insertIscrizione(String gara, String vettura){
-        String query = "INSERT INTO iscrizione(gara, vettura) VALUES " + makeAttributi(gara, vettura); 
+        String query = "INSERT INTO iscrizione(gara, vettura) VALUES " + makeAttributi(gara, vettura) + ";"; 
         return runUpdate(query);
     }
 
     //operazione 6: registrazione del risultato conseguito da ciascuna vettura iscritta ad una gara
     public List<Map<String, Object>> getVettureGara (String gara){
-        String query = "SELECT vettura FROM iscrizione WHERE gara = " + putApici(gara);
+        String query = "SELECT vettura FROM iscrizione WHERE gara = " + putApici(gara) + ";";
         return runQuery(query);
     }
 
@@ -134,25 +135,63 @@ public class OperationManager extends DBmanager{
         int result1, result2;
         
         if(motivoRitiro == null){
-            query1 = "UPDATE iscrizione SET esito = " + esito + " WHERE gara = " + putApici(gara) + " AND vettura = " + vettura;
-            query2 = "UPDATE vettura SET punti = punti + " + Integer.toString(esitoToPunti(Integer.parseInt(esito))) + " WHERE numeroGara = " + vettura;
+            query1 = "UPDATE iscrizione SET esito = " + esito + " WHERE gara = " + putApici(gara) + " AND vettura = " + vettura + ";";
+            query2 = "UPDATE vettura SET punti = punti + " + Integer.toString(esitoToPunti(Integer.parseInt(esito))) + " WHERE numeroGara = " + vettura + ";";
             result1 = runUpdate(query1);
             result2 = runUpdate(query2);
             //restituisce -1 se c'è un errore in una delle due query, altrimenti la simma dei valori
             return (result1 == -1 || result2 == -1)? -1 : result1 + result2;
         }
     
-        query1 = "UPDATE iscrizione SET motivoRitiro = " + putApici(motivoRitiro) + " WHERE gara = " + putApici(gara) + " AND vettura = " + vettura;
+        query1 = "UPDATE iscrizione SET motivoRitiro = " + putApici(motivoRitiro) + " WHERE gara = " + putApici(gara) + " AND vettura = " + vettura + ";";
         return runUpdate(query1);
     }
 
     //operazione 7: verifica della possibilità di montare una componente su una vettura
-
+    public int insertComponenteWithCheck(
+        String codice, 
+        String vettura, 
+        String dataInstall, 
+        String costo, 
+        String tipo, 
+        String nCilindri,
+        String tipoMotore,
+        String cilindrata,
+        String nMarce,
+        String peso,
+        String materiale,
+        String costruttore
+    ){
+        String query = "SELECT DISTINCT tipo FROM componente WHERE vettura = "+ vettura + ";";
+        List<Map<String, Object>> checkTable = runQuery(query);
+        boolean available = true;
+        for(Map<String, Object> map : checkTable){
+            if(tipo.equals(map.get("tipo"))){
+                available = false;
+                break;
+            }
+        }
+        if(available == true)
+            return insertComponenteNoCheck(
+                    codice, 
+                    vettura, 
+                    dataInstall, 
+                    costo, 
+                    tipo, 
+                    nCilindri, 
+                    tipoMotore, 
+                    cilindrata, 
+                    nMarce, 
+                    peso, 
+                    materiale, 
+                    costruttore);
+        return 0;
+    }
 
     //operazione 8: per ciascuna scuderia stampare la somma totale dei finanziamenti ricevuti
     public List<Map<String, Object>> getFinanziamentiScuderie (){
         String query = 
-            "SELECT scuderia, SUM(quotaFinanziamento) AS somma\r\n" + //
+                "SELECT scuderia, SUM(quotaFinanziamento) AS somma\r\n" + //
                 "FROM vettura JOIN pilota\r\n" + //
                 "ON vettura.numeroGara = pilota.vettura\r\n" + //
                 "WHERE quotaFinanziamento IS NOT NULL\r\n" + //
@@ -167,7 +206,7 @@ public class OperationManager extends DBmanager{
                 "FROM vettura LEFT JOIN pilota\r\n" + //
                 "ON vettura.numeroGara = pilota.vettura\r\n" + //
                 "GROUP BY scuderia;";
-        return query;
+        return runQuery(query);
     }
 
     //operazione 10: visualizzare i piloti che hanno vinto nel circuito di casa
@@ -200,7 +239,7 @@ public class OperationManager extends DBmanager{
 
     //operazione 12: stampa mensile dei costruttori compreso il numero di componenti che ha fornito
     public List<Map<String, Object>> getCostruttori (){
-        String query = "SELECT * FROM costruttore";
+        String query = "SELECT * FROM costruttore;";
         return runQuery(query);
     }
 
@@ -223,5 +262,18 @@ public class OperationManager extends DBmanager{
         return runQuery(query);
     }
 
+    //operazione 15: report che elenchi ciascuna scuderia sulla base del rapporto punti/minuti di gara, mediando tra le macchine appartenenti a ciascuna scuderia
+    public List<Map<String, Object>> getReportPuntiMinuti() {
+        String query = 
+                "SELECT scuderia, AVG(punti/tempo)\r\n" + //
+                "FROM vettura JOIN\r\n" + //
+                "\t(SELECT vettura, SUM(gara.durata) AS tempo\r\n" + //
+                "\tFROM gara JOIN iscrizione\r\n" + //
+                "\tON gara.nome = iscrizione.gara\r\n" + //
+                "\tGROUP BY vettura) AS tempoVettura\r\n" + //
+                "ON vettura.numeroGara = tempoVettura.vettura\r\n" + //
+                "GROUP BY scuderia;";
+        return runQuery(query);
+    }
 
 }
